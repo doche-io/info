6e90df66c1a540fcf499a57b02f4a833dbab746f
ES-Yukun <79987160+ES-Yukun@users.noreply.github.com>
2022-06-11 06:44:48 +0900

    Create 2.fc-ctrd.sh

---
+++ test/2.fc-ctrd.sh
@@ -0,0 +1,47 @@
+curl -fsSL -o hello-vmlinux.bin https://s3.amazonaws.com/spec.ccfc.min/img/quickstart_guide/x86_64/kernels/vmlinux.bin
+apt install -y ca-certificates curl gnupg lsb-release build-essential
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+git clone --recurse-submodules https://github.com/firecracker-microvm/firecracker-containerd
+cd firecracker-containerd
+  make all
+  make install
+  make firecracker
+  make install-firecracker
+  make image
+  mkdir -p /var/lib/firecracker-containerd/runtime
+  cp tools/image-builder/rootfs.img /var/lib/firecracker-containerd/runtime/default-rootfs.img
+cd ..
+cat <<EOF > ./config.toml
+version = 2
+disabled_plugins = ["io.containerd.grpc.v1.cri"]
+root = "/var/lib/firecracker-containerd/containerd"
+state = "/run/firecracker-containerd"
+[grpc]
+  address = "/run/firecracker-containerd/containerd.sock"
+[plugins]
+  [plugins."io.containerd.snapshotter.v1.devmapper"]
+    pool_name = "fc-dev-thinpool"
+    base_image_size = "10GB"
+    root_path = "/var/lib/firecracker-containerd/snapshotter/devmapper"
+
+[debug]
+  level = "debug"
+EOF
+cat <<EOF > /etc/containerd/firecracker-runtime.json
+{
+  "firecracker_binary_path": "/usr/local/bin/firecracker",
+  "kernel_image_path": "/var/lib/firecracker-containerd/runtime/hello-vmlinux.bin",
+  "kernel_args": "console=ttyS0 noapic reboot=k panic=1 pci=off nomodules ro systemd.unified_cgroup_hierarchy=0 systemd.journald.forward_to_console systemd.unit=firecracker.target init=/sbin/overlay-init",
+  "root_drive": "/var/lib/firecracker-containerd/runtime/default-rootfs.img",
+  "cpu_template": "T2",
+  "log_fifo": "fc-logs.fifo",
+  "log_levels": ["debug"],
+  "metrics_fifo": "fc-metrics.fifo"
+}
+EOF
+mkdir -p /var/lib/firecracker-containerd
